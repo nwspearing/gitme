4f65207a7c456020e16b38187d7c0be8840bc6a3
Tim Preston <tim@tp.id.au>
2013-11-06 09:18:47 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gitme::Application.config.secret_key_base = 'f28eddf2fb11709eacade82bd6ee8e6d7ae646d4f59037f1f5c46e83837628cacbda3b70ff22286ce77d0ebbebe674d68896630e238b0419a3971ebe2defee73'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gitme::Application.config.session_store :cookie_store, key: '_gitme_session'
